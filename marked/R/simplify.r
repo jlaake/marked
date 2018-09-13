3c5b62379034693244f70e4c9efea889536f16b9
Jeff Laake <jefflaake@gmail.com>
2018-09-13 12:02:02 -0700

    Switched to Devin's data.table approach for simplification

--- marked/R/simplify.r
+++ marked/R/simplify.r
@@ -1,35 +1,12 @@
-"realign.pims" <-function(dm){
-#  Value:
-#
-#  new.indices - a vector of new indices for the old PIM values.  The old
-#                PIM values are 1:length(new.indices) and the new index is
-#                the corresponding value.  For example, new.indices=c(1,1,2,2)
-#                renumbers the structure 1,2,3,4 such that 1,2 are now 1
-#                and 3,4 are now 2.
-#
-#  Get all the rows in the design matrix and paste all the values
-#  in each row together.
-#
-	allvals=apply(dm,1,paste,collapse="")
-#
-#  Get all the unique rows in the design matrix and paste all the values
-#  in each row together.
-#
-	uniquevals=unique(allvals)
-#
-#  Find the corresponding sets of indices by matching allvals into uniquevals
-#
-	new.indices=match(allvals, uniquevals)
-	
-
-	return(new.indices)
-}
 simplify_indices=function(x)
 {
-	uniquevals=apply(unique(x),1,paste,collapse="")
-	allvals=apply(x,1,paste,collapse="")
-	new.indices=match(allvals, uniquevals)
-	return(list(indices=new.indices,set=which(!duplicated(allvals))))
+    nms = colnames(x)
+    df=data.table(x)
+    u_rows=which(!duplicated(df))
+    u_df = df[u_rows,,drop=FALSE]
+    u_df$idx = 1:nrow(u_df)
+    out_idx = merge(df, u_df, by=nms, sort=F)
+    return(list(indices=out_idx$idx,set=u_rows))
 }
 simplify_ddl=function(ddl,parameters)
 {
@@ -42,8 +19,7 @@ simplify_ddl=function(ddl,parameters)
 		if(length(fields)==0)
 			slist=list(indices=rep(1,nrow(ddl[[parname]])),set=1)
 		else
-			slist=simplify_indices(cbind(ddl[[parname]][,fields]))
-		ddl[[paste(parname,"id",sep=".")]]=ddl[[parname]]$id
+			slist=simplify_indices(ddl[[parname]][,fields,drop=FALSE])
 		ddl[[parname]]=ddl[[parname]][slist$set,]
 		ddl[[paste(parname,"indices",sep=".")]]=slist$indices
 	}
