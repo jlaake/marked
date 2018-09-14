916428f0ae181e5b2b84a76df15a290abbacb82b
Jeff Laake <jefflaake@gmail.com>
2018-09-14 15:29:32 -0700

    cleaned up indents

--- marked/src/mvmsdmat.f
+++ marked/src/mvmsdmat.f
@@ -23,25 +23,25 @@ C         For first release occasion, p=1 and delta only applied if for initial
 C         release state could be uncertainty
           IF(J.EQ.F(I)) THEN
                 DO 5 K=1,NP
-				  ICOL=INDICES(K,2)
+				          ICOL=INDICES(K,2)
                   PMAT(I,J,INDICES(K,1),ICOL)=1.0D0 
   5             CONTINUE
                 PMAT(I,J,1,M)=1.0D0
                 IF(UNKINIT.EQ.1) THEN
                    DO 9 L=1,M-1
-		              COLSUM(L)=0.0D0
+		                 COLSUM(L)=0.0D0
   9                CONTINUE
                    INDEX=(J-1)*NP
                    DO 10 K=1,NP
                       INDEX=INDEX+1
                       ICOL=INDICES(K,2)
                       DMAT(INDICES(K,1),ICOL)=DELTA(I,INDEX)
-			          COLSUM(ICOL)=COLSUM(ICOL)+DELTA(I,INDEX)
+			                COLSUM(ICOL)=COLSUM(ICOL)+DELTA(I,INDEX)
  10                CONTINUE
                    DO 12 L=1,M-1
-  		           DO 12 K=2,NOBS
-			           DMAT(K,L)=DMAT(K,L)/COLSUM(L)
-			           PMAT(I,J,K,L)=PMAT(I,J,K,L)*DMAT(K,L)
+  		             DO 12 K=2,NOBS
+			                DMAT(K,L)=DMAT(K,L)/COLSUM(L)
+			                PMAT(I,J,K,L)=PMAT(I,J,K,L)*DMAT(K,L)
  12                CONTINUE
                 ENDIF             
            ELSE
@@ -52,25 +52,25 @@ C               Uses M-1 to exclude death state
                    ICOL=INDICES(K,2)
                    PMAT(I,J,INDICES(K,1),ICOL)=P(I,INDEX+ICOL)
                    PMAT(I,J,1,ICOL)=1-P(I,INDEX+ICOL)
-  13             CONTINUE
+  13            CONTINUE
                 PMAT(I,J,1,M)=1.0D0
 C               Compute values of delta IF there is uncertainty (UNK=1)
 C               Uses M-1 to exclude death state
                 IF(UNK.EQ.1) THEN
-		           DO 14 L=1,M-1
-		              COLSUM(L)=0.0D0
- 14                CONTINUE
-                   INDEX=(J-1)*NP
-                   DO 15 K=1,NP
+		                DO 14 L=1,M-1
+		                   COLSUM(L)=0.0D0
+ 14                 CONTINUE
+                    INDEX=(J-1)*NP
+                    DO 15 K=1,NP
                       INDEX=INDEX+1
                       ICOL=INDICES(K,2)
                       DMAT(INDICES(K,1),ICOL)=DELTA(I,INDEX)
-			          COLSUM(ICOL)=COLSUM(ICOL)+DELTA(I,INDEX)
+			                COLSUM(ICOL)=COLSUM(ICOL)+DELTA(I,INDEX)
  15                CONTINUE
                    DO 17 L=1,M-1
-  		           DO 17 K=2,NOBS
-			           DMAT(K,L)=DMAT(K,L)/COLSUM(L)
-			           PMAT(I,J,K,L)=PMAT(I,J,K,L)*DMAT(K,L)
+  		             DO 17 K=2,NOBS
+			               DMAT(K,L)=DMAT(K,L)/COLSUM(L)
+			               PMAT(I,J,K,L)=PMAT(I,J,K,L)*DMAT(K,L)
  17                CONTINUE
                 ENDIF
            ENDIF
